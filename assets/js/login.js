996a494a5f519786178ef467b8af870afc4c6d96
HuYanSiJun <1296680359@qq.com>
2020-12-06 14:49:01 +0800

    login页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,52 @@ $(function () {
   })
 
   let form = layui.form
+  let layer = layui.layer
   form.verify({
-  'pwd':[/^[\S]{6,12}$/,"密码必须6-12位,且不包含空格"]
+    pwd: [/^[\S]{6,12}$/, "密码必须6-12位,且不包含空格"],
+    repwd: function (value) {
+      let pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    }
+  })
+  // 注册
+  $('#form_reg').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/reguser',
+      data: {
+        username:$('#form_reg [name=username]').val(),
+        password:$('#form_reg [name=password]').val()
+      },
+      success: res => {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg(res.message)
+        $('#link_login').click()
+      }
+    })
+  })
+
+  $('#form_login').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/login',
+      data: $(this).serialize(),
+      success: res => {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg(res.message)
+        // console.log(res.token);
+        localStorage.setItem('token',res.token)
+        // location.href='../../index.html'
+        location.href='/index.html'
+      }
+    })
   })
 })
